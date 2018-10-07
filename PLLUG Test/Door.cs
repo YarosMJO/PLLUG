22d23023fd06e8a1dc5bfbd3787c44a500f575af
Mykhailivskyi Yaroslav <myhajlivej@gmail.com>
2018-10-07 15:36:37 +0300

    added files

---
+++ PLLUG Test/Door.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PLLUG_Test
+{
+    public sealed class Door
+    {
+        private static readonly Lazy<Door> lazy =  new Lazy<Door>(() => new Door());
+        public static Door Instance { get { return lazy.Value; } }
+
+        public const int HEIGHT = 220;
+        public const int WIDTH = 80;
+
+        private Door() {}
+    }
+}
---
+++ PLLUG Test/Fridge.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PLLUG_Test
+{
+    public class Fridge: Appliance
+    {
+        public Fridge(int height, int width, int length) : base(height, width, length) { }
+        public override void PrintStatus()
+        {
+            Console.WriteLine($"{nameof(Fridge)} {(CheckDimensions() ? "fit" : "not fit")}");
+        }
+    }
+}
